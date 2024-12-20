39b1c57b0c2a1bbb1e21070bc1a5a8dd26477c9e
Dylan McCormick <dylanm.bsa@gmail.com>
2024-12-20 16:51:11 -0500

    config save

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 --[[
-
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
@@ -91,7 +90,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +101,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -159,7 +158,7 @@ vim.opt.scrolloff = 10
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -540,7 +539,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -729,27 +728,105 @@ require('lazy').setup({
       }
     end,
   },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
-    priority = 1000, -- make sure to load this before all the other start plugins
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like
+  {
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        ---Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
+        ---Default is "medium"
+        background = "medium",
+        ---How much of the background should be transparent. 2 will have more UI
+        ---components be transparent (e.g. status line background)
+        transparent_background_level = 0,
+        ---Whether italics should be used for keywords and more.
+        italics = false,
+        ---Disable italic fonts for comments. Comments are in italics by default, set
+        ---this to `true` to make them _not_ italic!
+        disable_italic_comments = false,
+        ---By default, the colour of the sign column background is the same as the as normal text
+        ---background, but you can use a grey background by setting this to `"grey"`.
+        sign_column_background = "none",
+        ---The contrast of line numbers, indent lines, etc. Options are `"high"` or
+        ---`"low"` (default).
+        ui_contrast = "low",
+        ---Dim inactive windows. Only works in Neovim. Can look a bit weird with Telescope.
+        ---
+        ---When this option is used in conjunction with show_eob set to `false`, the
+        ---end of the buffer will only be hidden inside the active window. Inside
+        ---inactive windows, the end of buffer filler characters will be visible in
+        ---dimmed symbols. This is due to the way Vim and Neovim handle `EndOfBuffer`.
+        dim_inactive_windows = false,
+        ---Some plugins support highlighting error/warning/info/hint texts, by
+        ---default these texts are only underlined, but you can use this option to
+        ---also highlight the background of them.
+        diagnostic_text_highlight = false,
+        ---Which colour the diagnostic text should be. Options are `"grey"` or `"coloured"` (default)
+        diagnostic_virtual_text = "coloured",
+        ---Some plugins support highlighting error/warning/info/hint lines, but this
+        ---feature is disabled by default in this colour scheme.
+        diagnostic_line_highlight = false,
+        ---By default, this color scheme won't colour the foreground of |spell|, instead
+        ---colored under curls will be used. If you also want to colour the foreground,
+        ---set this option to `true`.
+        spell_foreground = false,
+        ---Whether to show the EndOfBuffer highlight.
+        show_eob = true,
+        ---Style used to make floating windows stand out from other windows. `"bright"`
+        ---makes the background of these windows lighter than |hl-Normal|, whereas
+        ---`"dim"` makes it darker.
+        ---
+        ---Floating windows include for instance diagnostic pop-ups, scrollable
+        ---documentation windows from completion engines, overlay windows from
+        ---installers, etc.
+        ---
+        ---NB: This is only significant for dark backgrounds as the light palettes
+        ---have the same colour for both values in the switch.
+        float_style = "bright",
+        ---Inlay hints are special markers that are displayed inline with the code to
+        ---provide you with additional information. You can use this option to customize
+        ---the background color of inlay hints.
+        ---
+        ---Options are `"none"` or `"dimmed"`.
+        inlay_hints_background = "none",
+        ---You can override specific highlights to use other groups or a hex colour.
+        ---This function will be called with the highlights and colour palette tables.
+        ---@param highlight_groups Highlights
+        ---@param palette Palette
+        on_highlights = function(highlight_groups, palette) end,
+        ---You can override colours in the palette to use different hex colours.
+        ---This function will be called once the base and background colours have
+        ---been mixed on the palette.
+        ---@param palette Palette
+        colours_override = function(palette) end,
+      })
+      require("everforest").load()
+      -- vim.cmd.colorscheme([[colorscheme everforest]])
       vim.cmd.hi 'Comment gui=none'
-    end,
+      vim.o.background = "light"
+    end
   },
-
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'everforest'
+  --
+  --     -- You can configure highlights by doing something like
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  --NOTE this is a note
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true } },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
